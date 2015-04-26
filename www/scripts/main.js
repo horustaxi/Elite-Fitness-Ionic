cff4fa0e9061b38ab3594eef053d303d66e73046
Coderhell <saparauskas@gmail.com>
2015-04-26 01:39:24 +0100

    Seperated css and js into seperate folders and added comments.

---
+++ www/scripts/main.js
@@ -0,0 +1,142 @@
+angular.module('ionicApp', ['ionic'])
+	
+	.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('eventmenu', {
+      url: "/event",
+      abstract: true,
+      templateUrl: "templates/event-menu.html"
+    })
+    .state('eventmenu.home', {
+      url: "/home",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/home.html"
+        }
+      }
+    })
+    .state('eventmenu.checkin', {
+      url: "/check-in",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/check-in.html",
+          controller: "CheckinCtrl"
+        }
+      }
+    })
+	
+	.state('eventmenu.test', {
+      url: "/test",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/test.html",
+          controller: "CheckinCtrl"
+        }
+      }
+    })
+	.state('eventmenu.calculator', {
+      url: "/calculator",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/calculator.html",
+          controller: "CheckinCtrl"
+        }
+      }
+    })
+    .state('eventmenu.attendees', {
+      url: "/attendees",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/attendees.html",
+          controller: "AttendeesCtrl"
+        }
+      }
+    })
+  
+  $urlRouterProvider.otherwise("/event/home");
+})
+
+.controller('MainCtrl', function($scope, $ionicSideMenuDelegate) {
+  $scope.attendees = [
+    { firstname: 'Nicolas', lastname: 'Cage' },
+    { firstname: 'Jean-Claude', lastname: 'Van Damme' },
+    { firstname: 'Keanu', lastname: 'Reeves' },
+    { firstname: 'Steven', lastname: 'Seagal' }
+  ];
+  
+  $scope.toggleLeft = function() {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+})
+
+.controller('CheckinCtrl', function($scope) {
+  $scope.showForm = true;
+  
+  $scope.shirtSizes = [
+    { text: 'Large', value: 'L' },
+    { text: 'Medium', value: 'M' },
+    { text: 'Small', value: 'S' }
+  ];
+  
+  $scope.attendee = {};
+  $scope.submit = function() {
+    if(!$scope.attendee.firstname) {
+      alert('Info required');
+      return;
+    }
+    $scope.showForm = false;
+    $scope.attendees.push($scope.attendee);
+  };
+  
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+  .state('intro', {
+    url: '/',
+    templateUrl: 'templates/intro.html',
+    controller: 'IntroCtrl'
+  })
+  .state('event-menu', {
+    url: '/event-menu',
+    templateUrl: 'templates/event-menu.html',
+    controller: 'MainCtrl'
+  });
+
+  $urlRouterProvider.otherwise("/");
+
+})
+
+.controller('IntroCtrl', function($scope, $state, $ionicSlideBoxDelegate) {
+ 
+  // Called to navigate to the event-menu app
+  $scope.startApp = function() {
+    $state.go('event-menu');
+  };
+  $scope.next = function() {
+    $ionicSlideBoxDelegate.next();
+  };
+  $scope.previous = function() {
+    $ionicSlideBoxDelegate.previous();
+  };
+
+  // Called each time the slide changes
+  $scope.slideChanged = function(index) {
+    $scope.slideIndex = index;
+  };
+})
+
+.controller('MainCtrl', function($scope, $state) {
+  console.log('MainCtrl');
+  
+  $scope.toIntro = function(){
+    $state.go('intro');
+  }
+});
+
+
+
+
+
